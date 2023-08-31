b6287795f9c0db07591810d5508885d952ed7ce3
aroraakash22 <143225075+aroraakash22@users.noreply.github.com>
2023-09-01 02:56:42 +0530

    Add files via upload

---
+++ Modified_Chatbot.py
@@ -0,0 +1,40 @@
+
+import time
+
+def typewriter_effect(text):
+    typed_text = ''
+    for char in text:
+        typed_text += char
+        time.sleep(0.05)
+    st.write(typed_text)
+import openai
+import streamlit as st
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    
+with st.sidebar:
+    directive = st.text_area("Directive for backend")
+"[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("PW Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    typewriter_effect(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
