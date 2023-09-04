180e365d090c825bf27715ee6ccb1a21c9a4b2c4
aroraakash22 <143225075+aroraakash22@users.noreply.github.com>
2023-09-04 14:55:43 +0530

    Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -22,7 +22,7 @@ if prompt := st.chat_input():
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo-0613:physicswallah::7uzz4KZf", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
